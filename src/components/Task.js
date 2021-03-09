7779f76b19af2e6774999dfcee81ebca541a5251
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-09 15:01:40 +0300

    Add edit button

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,5 @@
 import { FaTimes } from 'react-icons/fa';
+import { AiTwotoneEdit } from 'react-icons/ai';
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
@@ -6,8 +7,14 @@ const Task = ({ task, onDelete, onToggle }) => {
       className={`task ${task.reminder ? 'reminder' : ''}`}
       onDoubleClick={() => onToggle(task.id)}>
       <h3>
-        {task.text}{' '}
-        <FaTimes style={{ color: 'red' }} onClick={() => onDelete(task.id)} />
+        <div className='column-left'>{task.text}</div>
+        <div className='column-right'>
+          <AiTwotoneEdit />
+          <FaTimes
+            style={{ color: 'red', float: 'right' }}
+            onClick={() => onDelete(task.id)}
+          />
+        </div>
       </h3>
       <p>{task.date}</p>
     </div>
