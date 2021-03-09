7cb2a33633c29ee0f290121e4155b9b3381f9a9a
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-09 03:05:15 +0300

    Move the task to the App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctors Appointment',
+      day: 'Feb 5th at 2:30pm',
+      remainder: true,
+    },
+    {
+      id: 2,
+      text: 'Meeting at school',
+      day: 'Feb 6th at 1:30pm',
+      remainder: true,
+    },
+    {
+      id: 3,
+      text: 'Food Shopping',
+      day: 'Feb 5th at 2:30pm',
+      remainder: false,
+    },
+  ]);
+
   return (
     <div className='container'>
       <Header />
-      <h1>Hello From React</h1>
+      <Tasks tasks={tasks} />
     </div>
   );
 }
