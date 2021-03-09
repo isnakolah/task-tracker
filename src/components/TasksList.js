fb0f08a9ca30ff68be602d30ecc6c7dbe3d3436d
Daniel Nakolah <nakolahdaniel1@gmail.com>
2021-03-09 03:13:44 +0300

    Update component name from Task to TaskList

---
+++ src/components/TasksList.js
@@ -0,0 +1,13 @@
+import Task from './Task';
+
+const TasksList = ({ tasks }) => {
+  return (
+    <>
+      {tasks.map((task) => (
+        <Task key={task.id} task={task} />
+      ))}
+    </>
+  );
+};
+
+export default TasksList;
